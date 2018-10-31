dc4314d5343519a6c9ae991213115820d84b01bf
Rafael Santos <rafaelsantos.rse@gmail.com>
2018-10-31 16:11:36 -0300

    Final test

--- sources/add2vals.py
+++ sources/add2vals.py
@@ -22,7 +22,5 @@ if argnumbers != 2 :
     print("       If add2vals is not in your path, usage is './add2vals X Y'.")
     print("       If unbundled, usage is 'python add2vals.py X Y'.")
     print("")
-    print("Something random just to commit and push to the repository")
-    print("Something else just to commit again.")
-    print("I'm doing this again.")
+    print("fuck you all")
     sys.exit(1)
